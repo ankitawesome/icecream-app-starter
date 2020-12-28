2706dc4dd7d87608d1bf3edf6eb47051647b3e75
Ankit <akc9065@gmail.com>
2020-12-28 18:31:50 +0530

    added cart page and authentication routes

--- server.js
+++ server.js
@@ -12,15 +12,29 @@ const port = process.env.PORT || 3000
 //Assets
 app.use(express.static('public'))
 
-// A simple route to show home page
-app.get('/', (req, res)=>{
-    res.render('home')
-})
 //set template engine and configuration of it
 
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
+
+// A simple route to show home page
+app.get('/', (req, res)=>{
+    res.render('home')
+})
+
+app.get('/cart', (req, res)=>{
+    res.render('customers/cart')
+})
+
+app.get('/login', (req, res)=>{
+    res.render('auth/login')
+})
+
+app.get('/register', (req, res)=>{
+    res.render('auth/register')
+})
+
 
 // Listening the port on the port 
 app.listen(port, ()=>{
